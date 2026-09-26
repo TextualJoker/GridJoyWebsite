33e0b12aa0c2c7b37e9dad854b842dee4d8c1cab
TextualJoker <TextualJoker@outlook.com>
2026-09-26 05:33:29 +0100

    deploy: GridJoy website 2026-09-26 05:33:29

---
+++ assets/use-settled-win-CEHc2czs.js
@@ -0,0 +1,1 @@
+import{aW as n}from"./index-B-fA5wII.js";function u(t){const e=n();return t&&e}export{u};
---
+++ play/assets/self-grading-types-CYExZo_w.js
@@ -0,0 +1,1 @@
+const o=["sudoku","calcudoku","shikaku"];function a(l){return o.includes(l)}function n(l){return a(l)?{logicallySolvable:!0}:{}}export{n as l};
